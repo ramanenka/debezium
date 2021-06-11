eb58f30ff6b49d863cc3c4f07715b6a234b48253
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2021-06-11 18:09:34 +0300

    DBZ-3622: Skip DDLs of excluded databases

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlDatabaseSchemaTest.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlDatabaseSchemaTest.java
@@ -94,7 +94,7 @@ public class MySqlDatabaseSchemaTest {
         offset.setBinlogStartPoint("binlog-001", 400);
         mysql.parseStreamingDdl(partition, "SET " + MySqlSystemVariables.CHARSET_NAME_SERVER + "=utf8mb4", null,
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
-        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "connector_test",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
 
         // Check that we have tables ...
@@ -123,7 +123,7 @@ public class MySqlDatabaseSchemaTest {
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
         mysql.parseStreamingDdl(partition, "xxxCREATE TABLE mytable\n" + IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "db1",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
-        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "connector_test",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
 
         // Check that we have tables ...
@@ -148,7 +148,7 @@ public class MySqlDatabaseSchemaTest {
         offset.setBinlogStartPoint("binlog-001", 400);
         mysql.parseStreamingDdl(partition, "SET " + MySqlSystemVariables.CHARSET_NAME_SERVER + "=utf8mb4", null,
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
-        mysql.parseStreamingDdl(partition, "xxxCREATE TABLE mytable\n" + IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, "xxxCREATE TABLE mytable\n" + IoUtil.readClassPathResource("ddl/mysql-products.ddl"), "connector_test",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
     }
 
@@ -230,7 +230,7 @@ public class MySqlDatabaseSchemaTest {
 
         // Set up the server ...
         offset.setBinlogStartPoint("binlog-001", 400);
-        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-decimal-issue.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-decimal-issue.ddl"), "connector_test",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
 
         assertTableIncluded("connector_test.business_order");
@@ -253,7 +253,7 @@ public class MySqlDatabaseSchemaTest {
 
         // Set up the server ...
         offset.setBinlogStartPoint("binlog-001", 400);
-        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-schema-captured.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-schema-captured.ddl"), "captured",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
 
         assertTableIncluded("captured.ct");
@@ -285,7 +285,7 @@ public class MySqlDatabaseSchemaTest {
 
         // Set up the server ...
         offset.setBinlogStartPoint("binlog-001", 400);
-        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-schema-captured.ddl"), "db1",
+        mysql.parseStreamingDdl(partition, IoUtil.readClassPathResource("ddl/mysql-schema-captured.ddl"), "captured",
                 offset, Instant.now()).forEach(x -> mysql.applySchemaChange(x));
 
         assertTableIncluded("captured.ct");
